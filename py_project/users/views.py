f1e9f69a2aa7f534bb39bb227fff9d1f7b991eaf
sungmin-choi <53036141+sungmin-choi@users.noreply.github.com>
2020-08-02 22:23:09 +0800

    day 4

--- py_project/users/views.py
+++ py_project/users/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth import authenticate, login, logout, update_session_auth
 from django.contrib import messages
 from django.urls import reverse
 from .models import User
-from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from .form import JoinForm
+from django.contrib.auth.hashers import check_password
 
 # Create your views here.
 
@@ -56,21 +57,28 @@ def userDetail(request):
 def change_password(request):
     b = False
     if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            b = True
-            info = "변경성공"
-            context = {'info': info, 'b': b}
-            user = form.save()
-            update_session_auth_hash(request, user)  # Important!
-            return render(request, 'users/info.html', context)
+        current_password = request.POST.get("OriginPassword")
+        user = request.user
+        if check_password(current_password, user.password):
+            new_password = request.POST.get("setPassword1")
+            password_confirm = request.POST.get("setPassword2")
+            if new_password == password_confirm:
+                b = True
+                info = "변경성공"
+                context = {'info': info, 'b': b}
+                user.set_password(new_password)
+                user.save()
+                return render(request, 'users/info.html', context)
+            else:
+                b = False
+                info = "새로운 비밀번호가 맞지않습니다"
+                context = {'info': info, 'b': b}
+                return render(request, 'users/info.html', context)
         else:
             b = False
-            info = "비밀번호가 맞지않습니다"
+            info = "기존 비밀번호가 맞지않습니다"
             context = {'info': info, 'b': b}
             return render(request, 'users/info.html', context)
+
     else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'users/change_password.html', {
-        'form': form
-    })
+        return render(request, 'users/change_password.html')
